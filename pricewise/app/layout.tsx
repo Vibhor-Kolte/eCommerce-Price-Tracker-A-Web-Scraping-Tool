9cc3cf37b702629a36e25df9826c1eb5a0406038
Vibhor-Kolte <47689130+Vibhor-Kolte@users.noreply.github.com>
2024-01-06 17:24:01 +0530

    [Release_0.01] - Navbar Component  (#15)
    * [DEV-001] - NextJS Installation
    
    * [DEV-001]-Initial Setup
    
    * [DEV-002] - Add Navbar react component
    
    * [DEV-002] - Add icons and images useful for the project
    
    * [DEV-002] - Basic Navbar setup

---
+++ pricewise/app/layout.tsx
@@ -0,0 +1,38 @@
+import Navbar from '@/components/Navbar'
+
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter, Space_Grotesk } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({
+  subsets:['latin'], weight: ['300','400','500','600','700']
+})
+
+export const metadata: Metadata = {
+  title: 'Pricewise',
+  description: 'Track Product effotlessly',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+          <Navbar/>
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
+
+
+/*
+
+Allows sharing differnt parts of application across different pages
+*/
