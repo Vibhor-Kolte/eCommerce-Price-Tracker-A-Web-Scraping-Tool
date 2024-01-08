87f9959baa038aaf47208a6e2c19f8fb96974edb
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-09 04:42:12 +0530

    [DEV-003] - start of the content section of homepage

--- pricewise/app/page.tsx
+++ pricewise/app/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Image from "next/image"
 
 const Home = () => {
   return (
     <>
     <section className="px-6 md:px-20 py-24 border-2 border-red-500"> {/* On Medium Devices, define padding-x & padding-y */}
-
+      <div className="flex max-xl:flex-col gap-16"> {/* On Max-XL Devices, it will show more in column way */}
+        <div className="flex flex-col justify-center"> 
+          <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image 
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+        </div>
+      </div>
     </section>
     </>
   )
