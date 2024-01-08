a7e6bd3bc7405a6cf9eeffb98a0bdb7fe809a3d1
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-09 04:45:46 +0530

    [DEV-003] - Heading - Unleash the Power

--- pricewise/app/page.tsx
+++ pricewise/app/page.tsx
@@ -8,14 +8,24 @@ const Home = () => {
       <div className="flex max-xl:flex-col gap-16"> {/* On Max-XL Devices, it will show more in column way */}
         <div className="flex flex-col justify-center"> 
           <p className="small-text">
-              Smart Shopping Starts Here:
-              <Image 
-                src="/assets/icons/arrow-right.svg"
-                alt="arrow-right"
-                width={16}
-                height={16}
-              />
-            </p>
+            Smart Shopping Starts Here:
+            <Image 
+              src="/assets/icons/arrow-right.svg"
+              alt="arrow-right"
+              width={16}
+              height={16}
+            />
+          </p>
+
+          <h1 className="head-text">
+            Unleash the Power of
+            <span className="text-primary"> PriceWise</span>
+          </h1>
+
+          <p className="mt-6"> {/* margin top of 6*/}
+            Powerful, system to track your products of any eCommerce Site and make smarter decisions while buying.
+          </p>
+
         </div>
       </div>
     </section>
