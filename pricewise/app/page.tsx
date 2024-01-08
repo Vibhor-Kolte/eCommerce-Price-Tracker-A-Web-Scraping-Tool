32b32d6698c266e243a4c4c879ee37ac5f583b2c
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-09 04:37:28 +0530

    [DEV-003] - define section to have main content of the homepage

--- pricewise/app/page.tsx
+++ pricewise/app/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section className="px-6 md:px-20 py-24 border-2 border-red-500"> {/* On Medium Devices, define padding-x & padding-y */}
+
+    </section>
+    </>
   )
 }
 
