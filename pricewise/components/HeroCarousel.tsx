0e8bc0de2f75354d7c252c2b44d4c01ec44b998a
Vibhor-Kolte <47689130+Vibhor-Kolte@users.noreply.github.com>
2024-01-09 05:45:48 +0530

    [DEV-005] - Add HeroCarousel Component (#18)
    * [DEV-005] - create HeroCarousel Component
    
    * [DEV-005] - Define HeroCarousel to render product images
    
    * [DEV-005] - Style the HeroCarousel Component
    
    * [DEV-005] - The arrow image on top of carousel

---
+++ pricewise/components/HeroCarousel.tsx
@@ -0,0 +1,52 @@
+"use client"
+// https://www.npmjs.com/package/react-responsive-carousel
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+import Image from "next/image";
+
+const heroImages = [
+    { imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch'},
+    { imgUrl: '/assets/images/hero-2.svg', alt: 'bag'},
+    { imgUrl: '/assets/images/hero-3.svg', alt: 'lamp'},
+    { imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer'},
+    { imgUrl: '/assets/images/hero-5.svg', alt: 'chair'},
+  ]
+
+const HeroCarousel = () => {
+  return (
+    <div className="hero-carousel">
+        <Carousel
+            // Props for Carousel
+            showThumbs={false}
+            // autoPlay
+            infiniteLoop
+            // interval={2000}
+            showArrows={false}
+            showStatus={false}
+        >
+            {heroImages.map((image) => (
+            <Image 
+                src={image.imgUrl}
+                alt={image.alt}
+                width={484}
+                height={484}
+                className="object-contain"
+                key={image.alt}
+            />  // Error: Super expression must either be null or a function, not undefined {.\node_modules\react-easy-swipe\lib\react-swipe.js}
+            //  It is due to we haven't given any props and also it should be client side rendered 
+            // as it deals with interactivity, as react-responsive-carousel is using onClick(), onChanges() BTS.          
+            ))}
+        </Carousel>
+
+        <Image 
+            src="assets/icons/hand-drawn-arrow.svg"
+            alt="arrow"
+            width={175}
+            height={175}
+            className="max-xl:hidden absolute -left-[15%] bottom-0 z-0"
+        />
+    </div>
+  )
+}
+
+export default HeroCarousel
