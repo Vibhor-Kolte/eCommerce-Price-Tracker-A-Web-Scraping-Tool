0bda37a83de90626bf9c3170f18293d2c04bfeca
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-09 05:39:40 +0530

    [DEV-005] - Define HeroCarousel to render product images

--- pricewise/components/HeroCarousel.tsx
+++ pricewise/components/HeroCarousel.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+"use client"
+// https://www.npmjs.com/package/react-responsive-carousel
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+import Image from "next/image";
+
+const heroImages = [
+    { imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch'},
+    { imgUrl: '/assets/images/hero-2.svg', alt: 'bag'},
+    { imgUrl: '/assets/images/hero-3.svg', alt: 'lamp'},
+    { imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer'},
+    { imgUrl: '/assets/images/hero-5.svg', alt: 'chair'},
+  ]
 
 const HeroCarousel = () => {
   return (
-    <div>HeroCarousel</div>
+    <div>
+        <Carousel
+            // Props for Carousel
+            showThumbs={false}
+            // autoPlay
+            infiniteLoop
+            // interval={2000}
+            showArrows={false}
+            showStatus={false}
+        >
+            {heroImages.map((image) => (
+            <Image 
+                src={image.imgUrl}
+                alt={image.alt}
+                width={484}
+                height={484}
+                className="object-contain"
+                key={image.alt}
+            />  // Error: Super expression must either be null or a function, not undefined {.\node_modules\react-easy-swipe\lib\react-swipe.js}
+            //  It is due to we haven't given any props and also it should be client side rendered 
+            // as it deals with interactivity, as react-responsive-carousel is using onClick(), onChanges() BTS.          
+            ))}
+        </Carousel>
+    </div>
   )
 }
 
