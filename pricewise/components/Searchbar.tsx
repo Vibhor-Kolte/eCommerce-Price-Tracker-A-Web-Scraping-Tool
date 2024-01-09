26cd963fb21d6a6f3f49dc8b0d5a9f892434d6fb
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-01-09 06:06:04 +0530

    [DEV-006] - keep track of url that we submit using useState()

--- pricewise/components/Searchbar.tsx
+++ pricewise/components/Searchbar.tsx
@@ -1,10 +1,38 @@
-"use client"  
+"use client"
+import { FormEvent, useState } from "react";
+
+  
 // client side rendering, whereas other components are server side rendered
 // refer - https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns
 
-const handleSubmit = () => {}
-
+const isValidAmazonProductURL = (url: string) => {
+    try {
+      const parsedURL = new URL(url);
+      const hostname = parsedURL.hostname;
+  
+      if(hostname.includes('amazon.com') || hostname.includes ('amazon.') || hostname.endsWith('amazon')) {
+        return true;
+      }
+    } catch (error) {
+      return false;
+    }
+  
+    return false;
+  }
+  
 const Searchbar = () => {
+    // need to keep track of url that we submit
+    const [searchPrompt, setSearchPrompt] = useState('');
+    
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    
+        const isValidLink = isValidAmazonProductURL(searchPrompt);
+    
+        alert(isValidLink ? 'Valid Link':'Please provide a valid Amazon link');
+        // try:{https://amazon.com/mac}, {amazon.com/mac}
+    }
+
   return (
     <form 
       className="flex flex-wrap gap-4 mt-12" 
@@ -14,6 +42,8 @@ const Searchbar = () => {
             type="text"
             placeholder="Enter product link"
             className="searchbar-input"
+            value={searchPrompt}
+            onChange={(e) => setSearchPrompt(e.target.value)}   //keep track of our value within our state
         />
 
         <button 
