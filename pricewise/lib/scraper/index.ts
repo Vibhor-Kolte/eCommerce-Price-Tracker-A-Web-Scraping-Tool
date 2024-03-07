39a5a021893bdcbf0fd6197ef26b33b574149214
Vibhor-Kolte <47689130+Vibhor-Kolte@users.noreply.github.com>
2024-03-07 12:57:47 +0530

    [DEV-009] - Fetch product page and extract details (#23)
    * [DEV-009] - enable Server Actions
    
    * [DEV-009] - Fetch the product page and extract title
    
    * [DEV-009] - extract prices of the product

--- pricewise/lib/scraper/index.ts
+++ pricewise/lib/scraper/index.ts
@@ -1,4 +1,6 @@
-
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
@@ -6,7 +8,7 @@ export async function scrapeAmazonProduct(url: string) {
     // BrightData proxy configuration
     const username = String(process.env.BRIGHT_DATA_USERNAME);
     const password = String(process.env.BRIGHT_DATA_PASSWORD);
-    const port = 22225;
+    const port = 22225; // use env variable here
     const session_id = (1000000 * Math.random()) | 0;
 
     const options = {
@@ -21,10 +23,28 @@ export async function scrapeAmazonProduct(url: string) {
 
     try {
         // Fetch the product page
-
+        const response = await axios.get(url, options);
+        // console.log(response.data);
+        const $ = cheerio.load(response.data);
     
         // Extract the product title
-       
+        const title = $('#productTitle').text().trim();
+
+        const currentPrice = extractPrice(
+          $('.priceToPay span.a-price-whole'),
+          $('.a.size.base.a-color-price'),
+          $('.a-button-selected .a-color-base'),
+        );
+    
+        const originalPrice = extractPrice(
+          $('#priceblock_ourprice'),
+          $('.a-price.a-text-price span.a-offscreen'),
+          $('#listPrice'),
+          $('#priceblock_dealprice'),
+          $('.a-size-base.a-color-price')
+        );
+
+        console.log(title, currentPrice, originalPrice);
     
         // Construct data object with scraped information
         
