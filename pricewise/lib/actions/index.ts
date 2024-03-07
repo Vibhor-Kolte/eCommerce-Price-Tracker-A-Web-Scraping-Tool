dc0c6ecbb2a2797c0870b1b6525d78c2d3d44a4f
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-03-07 14:55:30 +0530

    [DEV-012] - Update priceHistory if product found in DB

--- pricewise/lib/actions/index.ts
+++ pricewise/lib/actions/index.ts
@@ -20,6 +20,21 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
     const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
+    if(existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice }
+      ]
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      }
+    }
+
     } catch (error: any) {
       throw new Error(`Failed to create/update product: ${error.message}`)
     }
