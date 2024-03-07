2b10bdff65db74c79017a37ff77dee3451d76d05
Vibhor-Kolte <47689130+Vibhor-Kolte@users.noreply.github.com>
2024-03-07 15:02:30 +0530

    [DEV-012] - Store and Update Product in MongoDB (#27)
    * [DEV-012] - Update priceHistory if product found in DB
    
    * [DEV-012] - Create new or update existing Product in DB
    
    * [DEV-012] - revalidate path

--- pricewise/lib/actions/index.ts
+++ pricewise/lib/actions/index.ts
@@ -1,9 +1,11 @@
 "use server"
+import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 // runs only on server
 import { connectToDB } from "../mongoose";
 
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
@@ -20,6 +22,28 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
     const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
+    if(existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice }
+      ]
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      }
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`);
     } catch (error: any) {
       throw new Error(`Failed to create/update product: ${error.message}`)
     }
