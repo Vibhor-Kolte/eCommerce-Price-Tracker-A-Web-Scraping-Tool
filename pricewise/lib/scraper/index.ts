66fc6f620203c53640eb914091fc39f8da54a27a
Vibhor-Kolte <vibhorkolte@gmail.com>
2024-03-07 12:52:30 +0530

    [DEV-009] - extract prices of the product

--- pricewise/lib/scraper/index.ts
+++ pricewise/lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
@@ -28,7 +29,22 @@ export async function scrapeAmazonProduct(url: string) {
     
         // Extract the product title
         const title = $('#productTitle').text().trim();
-        console.log(title);
+
+        const currentPrice = extractPrice(
+          $('.priceToPay span.a-price-whole'),
+          $('.a.size.base.a-color-price'),
+          $('.a-button-selected .a-color-base'),
+        );
+    
+        const originalPrice = extractPrice(
+          $('#priceblock_ourprice'),
+          $('.a-price.a-text-price span.a-offscreen'),
+          $('#listPrice'),
+          $('#priceblock_dealprice'),
+          $('.a-size-base.a-color-price')
+        );
+
+        console.log(title, currentPrice, originalPrice);
     
         // Construct data object with scraped information
         
